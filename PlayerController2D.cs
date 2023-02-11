318570a05d551a191aa43b5eddb773464148fed5
Marvin <70090867+MarvinTMavee@users.noreply.github.com>
2023-02-11 20:15:05 +0100

    Update and rename PlayerController.cs to PlayerController2D.cs

--- PlayerController2D.cs
+++ PlayerController2D.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController2D : MonoBehaviour
 {
 
     /*How to implement:
